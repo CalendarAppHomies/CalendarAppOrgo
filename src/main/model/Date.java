206aab963765de3d97a1ceeedf7b253b727fa809
sophiaswchu <94317776+sophiaswchu@users.noreply.github.com>
2021-11-14 13:27:21 -0800

    date

--- src/main/model/Date.java
+++ src/main/model/Date.java
@@ -4,6 +4,7 @@ public class Date {
     private Integer month;
     private Integer day;
 
+    // REQUIRES: a valid date
     public Date(int month, int day) {
         this.month = month;
         this.day = day;
@@ -15,5 +16,27 @@ public class Date {
 
     public Integer getMonth() {
         return month;
+    }
+
+    // EFFECTS: returns true if this is earlier in year than d
+    public boolean before(Date d) {
+        if (month < d.getMonth()) {
+            return true;
+        } else if (month == d.getMonth() && day <= d.getDay()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // EFFECTS: returns true if this is later in year than d
+    public boolean after(Date d) {
+        if (month > d.getMonth()) {
+            return true;
+        } else if (month == d.getMonth() && day > d.getDay()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
